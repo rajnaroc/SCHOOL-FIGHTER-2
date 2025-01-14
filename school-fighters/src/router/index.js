b0413550539427352939417603f6a23cb922cdfc
Aaron Campos <aaroncm611@gmail.com>
2025-01-14 10:58:44 +0100

    añadiendo rutas y paginas de la app

--- school-fighters/src/router/index.js
+++ school-fighters/src/router/index.js
@@ -1,19 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import VistaPersonajes from '@/views/VistaPersonajes.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'VistaPersonaje',
+    component: VistaPersonajes
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/VistaEscenario',
+    name: 'VistaEscenario',
+    component: () => import('@/views/VistaEscenario.vue')
+  },
+  {
+    path: '/VistaCombate',
+    name: 'VistaCombate',
+    component: () => import('@/views/VistaCombate.vue')
   }
 ]
 
